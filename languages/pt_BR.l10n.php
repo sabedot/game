386abdaaf540809580542764e9a5929a8a533dbb
sabedot <166958926+sabedot@users.noreply.github.com>
2025-07-11 01:42:12 -0300

    Add files via upload

---
+++ languages/pt_BR.l10n.php
@@ -0,0 +1,2 @@
+<?php
+return ['project-id-version'=>'FutStream Theme','report-msgid-bugs-to'=>'','pot-creation-date'=>'2025-07-07 01:42+0000','po-revision-date'=>'2025-07-07 01:42+0000','last-translator'=>'','language-team'=>'Português do Brasil','language'=>'pt_BR','plural-forms'=>'nplurals=2; plural=n != 1;','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','x-generator'=>'Loco https://localise.biz/','x-loco-version'=>'2.8.0; wp-6.8.1; php-7.4.33','x-domain'=>'futstream','messages'=>[]];
